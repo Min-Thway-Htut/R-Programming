8a1f52f0a097ebeed923795c35abc86d846a521d
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-08-20 12:47:33 +0200

    Second Update

---
+++ Learning-folder/second-R-program.R
@@ -0,0 +1,27 @@
+name <- "John"
+age  <- 40
+
+
+for (x in 1:10){
+  print(x)
+}
+  
+text <- "awesome"
+
+paste("R is", text)
+
+text1 <- "R is"
+text2 <- "awesome"
+
+paste(text1, text2)
+
+num1 <- 5
+num2 <- 10
+
+num1 + num2
+
+var1 <- var2 <- var3 <- "Orange"
+
+var1
+var2
+var3
